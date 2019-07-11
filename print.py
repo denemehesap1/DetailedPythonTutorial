74e02013da72d7fa24fe8dbed45e1dd2711d9838
denemehesap1 <52795501+denemehesap1@users.noreply.github.com>
2019-07-11 19:42:54 +0300

    Update print.py
    awdadawdawd

--- print.py
+++ print.py
@@ -5,3 +5,5 @@ print("Diğer Programlama dilleri: C++, C# ve Java")
 print("Python ile bir çok proje geliştirilebilir")
 
 print("Yeni print fonksiyonu eklendi from denemehesap1")
+
+print("ikinci yeni fonksiyon eklendi from denemehesap1")
